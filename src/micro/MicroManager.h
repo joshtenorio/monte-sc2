230d17b771b59736b70025052d31f4482d09601f
Josh T <joshliam09@gmail.com>
2021-07-10 17:43:35 -0700

    feat: created virtual class for micromanager

--- src/micro/MicroManager.h
+++ src/micro/MicroManager.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include <vector>
+#include<sc2api/sc2_unit.h>
+
+namespace Monte {
+    // first: tag ; second: state
+    typedef std::pair<sc2::Tag, int> Unit;
+} // end namespace
 
 class MicroManager {
     public:
-    int foo;
-    protected:
-
+    MicroManager() {};
+    virtual void doStateAction(Monte::Unit& unit) = 0;
+    virtual int validateState(Monte::Unit& unit) = 0;
 };
