5d10e678f3a42ad6b92c9ffdb90302f1fcfa5a6c
Josh T <joshliam09@gmail.com>
2021-07-17 15:49:49 -0700

    feat: started working on vector math functions

---
+++ src/VectorMath.h
@@ -0,0 +1,65 @@
+/**
+ * @file VectorMath.h
+ * @author Joshua Tenorio
+ * 
+ * Helper functions and implementations for vector math
+ */
+#pragma once
+
+#include <sc2api/sc2_common.h>
+
+namespace Monte {
+
+typedef struct Vector2D_s_t {
+    Vector2D_s_t() {};
+    Vector2D_s_t(float dx_, float dy_) { dx = dx_; dy = dy_; };
+    float dx, dy;
+
+    virtual float getMagnitude();
+    virtual float getMagnitudeSquared();
+
+    void operator = (const Vector2D_s_t& u){
+        dx = u.dx;
+        dy = u.dy;
+    }
+
+    bool operator == (const Vector2D_s_t& u) const {
+        if(
+            dx == u.dx &&
+            dy == u.dy
+        ) return true;
+        else return false;
+    }
+} Vector2D;
+
+typedef struct Vector3D_s_t : public Vector2D_s_t {
+    float dz;
+
+    void operator = (const Vector3D_s_t& u){
+        dx = u.dx;
+        dy = u.dy;
+        dz = u.dz;
+    }
+
+    bool operator == (const Vector3D_s_t& u) const {
+        if(
+            dx == u.dx &&
+            dy == u.dy &&
+            dz == u.dz
+        ) return true;
+        else return false;
+    }
+} Vector3D;
+
+// TODO: should this be in the vector struct ? if so it shouldnt change the vector struct
+// returns the unit vector of a given vector u
+Vector2D getUnitVector2D(Vector2D u);
+Vector3D getUnitVector3D(Vector3D u);
+
+// gets a point in the direction of a vector with a specified magnitude and initial point
+sc2::Point2D getPoint2D(sc2::Point2D initial, Vector2D direction, float magnitude);
+sc2::Point3D getPoint3D(sc2::Point3D initial, Vector3D direction, float magnitude);
+
+
+
+} // end namespace Monte
