830897974bba098fc17672c158ede1a698cab78e
Josh T <joshliam09@gmail.com>
2021-07-20 00:03:56 -0700

    feat: need to rethink tree and node implementation to make implementing functions less of a headache

--- src/PlacementTree.cpp
+++ src/PlacementTree.cpp
@@ -1,7 +1,75 @@
+#include "api.h" // we need access to query
 #include "PlacementTree.h"
 
 namespace Monte {
 
+// create n newNodes
+// TODO: we need to use Query in addition to checking reserved tiles until we update reservedTiles to make unbuildable destroyables reserved
+// we should reserve locations we find
+// TODO: we should rethink tree implementation bc current structure of expand is pretty janky and not gonna go well i think
+//          so we should do some lit review on tree implementations probably. at the very least we need a new structure for expand()
+//          we probably require some change in PTNode, maybe it should keep track of its level and how many children it has?
+void PlacementTree::expand(std::vector< std::vector< bool >>& reservedTileMap, int newNodes){
+    // start at root
+    PTNode* itr = root;
+    int createdNodes = 0;
+    while(createdNodes < newNodes){
+        // TODO: should we have a max number of loops to make sure loop has an upper bound?
+        // when we create a new node:
+        // if it is PTNODE_FREE, we should reserve the tiles and increment createdNodes
+        // if it is PTNODE_TERMINAL, we should *not* reserve the tiles and increment createdNodes
+        if(itr->n == nullptr){
+            // get the north location, double check the math (but pretty sure it works)
+            sc2::Point2D north = sc2::Point2D(itr->location.x, itr->location.y + radius * 2 + yMargin);
+            itr->n = addNode(north);
+            if(itr->n->status == PTNODE_FREE){
+                // reserve tiles here
+            }
+            createdNodes++;
+        }
+        else if(itr->s == nullptr){
 
+            createdNodes++;
+        }
+        else if(itr->e == nullptr){
+
+            createdNodes++;
+        }
+        else if(itr->w == nullptr){
+
+            createdNodes++;
+        }
+        else{
+            // if all 4 child nodes are not nullptr
+        }
+    }
+}
+
+// protected members //
+
+PTNode* PlacementTree::addNode(sc2::Point2D loc){
+    PTNode* newNode = new PTNode(loc);
+    newNode->n = nullptr;
+    newNode->s = nullptr;
+    newNode->e = nullptr;
+    newNode->w = nullptr;
+    newNode->status = PTNODE_FREE;
+
+    // get query for location, make sure it is valid
+    std::vector<sc2::QueryInterface::PlacementQuery> queries;
+    std::vector<bool> results;
+    queries.emplace_back(sc2::ABILITY_ID::BUILD_BARRACKS, loc);
+    if(isArmyBuilding)
+        queries.emplace_back(sc2::ABILITY_ID::BUILD_SUPPLYDEPOT, sc2::Point2D(loc.x + 2.5, loc.y - 0.5));
+    
+    results = gInterface->query->Placement(queries);
+    for(auto r : results)
+        if(!r)
+            newNode->status = PTNODE_TERMINAL;
+    
+
+    
+    return newNode;
+}
 
 } // end namespace Monte
--- src/PlacementTree.h
+++ src/PlacementTree.h
@@ -10,9 +10,9 @@
 #include <vector>
 
 // TODO: since this is used in a Monte namespace, perhaps this should be enum instead
-#define PTNODE_FREE     0
-#define PTNODE_TAKEN    1
-#define PTNODE_INVALID  2
+#define PTNODE_FREE     0 // the node's location is open and available
+#define PTNODE_TAKEN    1 // the node's location is used
+#define PTNODE_TERMINAL  2 // the node's location is invalid
 
 namespace Monte {
 
@@ -21,40 +21,47 @@ typedef struct PlacementTreeNode_s_t {
     // the building location of the node
     sc2::Point2D location;
 
-    char status = PTNODE_INVALID;
+    char status = PTNODE_TERMINAL;
 
     // pointers to children nodes in the tree
-    PTNode* n;
-    PTNode* s;
-    PTNode* e;
-    PTNode* w;
+    PTNode* n, *s, *e, *w;
 
 } PTNode;
 
+// TODO: should we refactor so it uses a queue as an underlying data structure?
 class PlacementTree {
     public:
     PlacementTree() {};
-    PlacementTree(sc2::Point2D loc, size_t maxLevels_) { root = new PTNode(loc); maxLevels = maxLevels_; };
+    PlacementTree(sc2::Point2D loc, size_t maxLevels_, bool isArmyBuilding_, int xMargin_, int yMargin_)
+        { root = new PTNode(loc); maxLevels = maxLevels_; isArmyBuilding = isArmyBuilding_; xMargin = xMargin; yMargin = yMargin; };
 
-    // create more branches
+    // create n newNodes
     // TODO: we need to use Query in addition to checking reserved tiles until we update reservedTiles to make unbuildable destroyables reserved
-    void expand(std::vector< std::vector< bool >> reservedTileMap, int numLoops);
+    // we should reserve locations we find
+    void expand(std::vector< std::vector< bool >>& reservedTileMap, int newNodes = 8);
 
-    // if a node's location is invalid, prune it and its children
+    // if a node's location is invalid, prune the node and its children
     void validate();
     
-    // TODO: DFS might be best
+    // TODO: DFS might be best but BFS could be better for performance/memory reasons
     sc2::Point2D getNextLocation();
 
     protected:
     // given a PTNode, delete it and its children
     // TODO: do we need to give this function the parent of n instead?
+    PTNode* addNode(sc2::Point2D loc);
     void pruneBranch(PTNode* n);
 
     PTNode* root;
-    size_t maxLevels;
-    // TODO: add instance variables for how much spacing we want between buildings
-    // TODO: add instance variable here for the radius of the building (perhaps safe to assume 1.5?)
+    size_t maxLevels; // maximum height of tree
+    bool isArmyBuilding; // if true, then reserve space for addon as well
+
+    // these two define how much space we want between buildings
+    // if margin = 0, then there is no space between buildings. if margin = 1, siege tanks cant fit through the gap
+    int xMargin;
+    int yMargin;
+    // TODO: add instance variable here for the radius of the building (or perhaps safe to assume 1.5?)
+    float radius = 1.5;
 };
 
 } // end namespace Monte
