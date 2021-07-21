f72b1812491843732fd0795eae542f5b6d900227
Josh T <joshliam09@gmail.com>
2021-07-21 16:01:06 -0700

    feat: declared functions for abstract Squad class

--- src/Squad.h
+++ src/Squad.h
@@ -1,6 +1,33 @@
 #pragma once
 
+#include "api.h"
+
 class Squad {
     public:
     Squad();
-}
+
+    void OnStep();
+
+    void absorb(Squad& squad);
+    sc2::Point2D getCenter();
+    
+    // TODO: add parameters
+    void addUnit();
+    void addUnits();
+    void removeUnit();
+    void disband();
+
+    void setTarget(bool attack); // TODO: do we need more parameters?
+    sc2::Point2D getTarget();
+    
+    protected:
+    void calculateCenter();
+    void validateState();
+
+    sc2::Point2D center;
+    sc2::Point2D target;
+
+    char state; // TODO: this necessary?
+
+    // TODO: put a list of Monte::Units here that represents the units the squad has control over
+};
