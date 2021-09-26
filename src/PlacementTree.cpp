734dfea9364a713cd6dd57893faf07a4cfe199e5
Josh T <joshliam09@gmail.com>
2021-09-25 18:10:20 -0700

    feat!: restarting placement trees with new approach

--- src/PlacementTree.cpp
+++ src/PlacementTree.cpp
@@ -3,117 +3,12 @@
 
 namespace Monte {
 
-// create n newNodes
-// TODO: we need to use Query in addition to checking reserved tiles until we update reservedTiles to make unbuildable destroyables reserved
-// TODO: we should rethink tree implementation bc current structure of expand is pretty janky and not gonna go well i think
-//          so we should do some lit review on tree implementations probably. at the very least we need a new structure for expand()
-void PlacementTree::expand(std::vector< std::vector< bool >>& reservedTileMap, int newNodes){
-    // start at root
-    PTNode* itr = root;
-    int createdNodes = 0;
-    while(createdNodes < newNodes){
-        if(itr == nullptr) return; // TODO: what should we do?
-
-        if(itr->status != PTNODE_TERMINAL){
-            if(itr->n == nullptr){
-                // create the north location
-                sc2::Point2D north = sc2::Point2D(itr->location.x, itr->location.y + radius * 2 + yMargin);
-                itr->n = addNode(north);
-                itr->n->level = itr->level + 1;
-                itr->numChildren++;
-                createdNodes++;
-                if(itr->n->status == PTNODE_FREE){
-                    // reserve tiles
-                    for(int x = north.x - radius; x < north.x + radius; x++)
-                        for(int y = north.y - radius; y < north.y + radius; y++)
-                            reservedTileMap[x][y] = true;
-                        
-                    // reserve space for addon
-                    if(isArmyBuilding){
-                        int xMin = north.x + 1.5, xMax = north.x + 3.5; //  1.5 + 2
-                        int yMin = north.y - 1.5, yMax = north.y + 0.5; // -1.5 + 2
-                        for(int x = xMin; x < xMax; x++)
-                            for(int y = yMin; y < yMax; y++)
-                                reservedTileMap[x][y] = true;
-
-                    }
-                } // end if itr->n->status == PTNODE_FREE
-            }
-            else if(itr->s == nullptr){
-                itr->numChildren++;
-                createdNodes++;
-            }
-            else if(itr->e == nullptr){
-                itr->numChildren++;
-                createdNodes++;
-            }
-            else if(itr->w == nullptr){
-                itr->numChildren++;
-                createdNodes++;
-            }
-            else{
-                // if all 4 child nodes are not nullptr
-                // pick a direction to expand to
-                PTNode* newDirection = nullptr;
-                int smallest = 99999; // TODO: replace this with actual max int value
-                if(itr->n->branchStatus == PTBRANCH_OPEN && itr->n->numChildren < smallest){
-                    smallest = itr->n->numChildren;
-                    newDirection = itr->n;
-                }
-                if(itr->s->branchStatus == PTBRANCH_OPEN && itr->s->numChildren < smallest){
-                    smallest = itr->s->numChildren;
-                    newDirection = itr->s;
-                }
-                if(itr->e->branchStatus == PTBRANCH_OPEN && itr->e->numChildren < smallest){
-                    smallest = itr->e->numChildren;
-                    newDirection = itr->e;
-                }
-                if(itr->w->branchStatus == PTBRANCH_OPEN && itr->w->numChildren < smallest){
-                    smallest = itr->w->numChildren;
-                    newDirection = itr->w;
-                }
-                if(newDirection != nullptr)
-                    itr = newDirection;
-                else return;
-            }
-        } // end if itr->status != PTNODE_TERMINAL
-        
-
-    } // end while createdNodes < newNodes
+sc2::Point2D PlacementTree::findPlacement(sc2::Point2D root, std::vector< std::vector< bool >>& reservedTileMap,
+                                        int maxDepth, int xMargin, int yMargin, bool isArmyBuilding){
+    // hehe
 }
 
-// protected members //
-
-PTNode* PlacementTree::addNode(sc2::Point2D loc){
-    PTNode* newNode = new PTNode(loc);
-    newNode->n = nullptr;
-    newNode->s = nullptr;
-    newNode->e = nullptr;
-    newNode->w = nullptr;
-    newNode->status = PTNODE_FREE;
-    newNode->branchStatus = PTBRANCH_OPEN;
-
-    // get query for location, make sure it is valid
-    std::vector<sc2::QueryInterface::PlacementQuery> queries;
-    std::vector<bool> results;
-    queries.emplace_back(sc2::ABILITY_ID::BUILD_BARRACKS, loc);
-    if(isArmyBuilding)
-        queries.emplace_back(sc2::ABILITY_ID::BUILD_SUPPLYDEPOT, sc2::Point2D(loc.x + 2.5, loc.y - 0.5));
-    
-    results = gInterface->query->Placement(queries);
-    for(auto r : results)
-        if(!r){
-            newNode->status = PTNODE_TERMINAL;
-            newNode->branchStatus = PTBRANCH_TERMINAL;
-        }
-            
-    
-
-    
-    return newNode;
-}
-
-void PlacementTree::pruneBranch(PTNode* n){
+bool PlacementTree::isValidPlacement(sc2::Point2D location, std::vector< std::vector< bool >>& reservedTileMap, bool isArmyBuilding){
     
 }
 
--- src/PlacementTree.h
+++ src/PlacementTree.h
@@ -9,65 +9,24 @@
 #include <sc2api/sc2_common.h>
 #include <vector>
 
-// TODO: since this is used in a Monte namespace, perhaps this should be enum instead
-#define PTNODE_FREE      0 // the node's location is open and available
-#define PTNODE_TAKEN     1 // the node's location is used
-#define PTNODE_TERMINAL  2 // the node's location is invalid
-
-#define PTBRANCH_TERMINAL 0 // the branch has been fully explored
-#define PTBRANCH_OPEN     1 // the branch is not yet fully explored
-
+/*
+when needing to place a building using placementTree,
+intake a sc2point2d (which should be a valid placement for building)
+and find the closest valid position and return it
+if there are no valid positions found, return something
+to let buildingplacer know, so that it can generate a new "root" position
+note: use recursion!
+by doing so we won't have to store anything long term :)))))
+*/
 namespace Monte {
 
-typedef struct PlacementTreeNode_s_t {
-    PlacementTreeNode_s_t(sc2::Point2D location_) { location = location_; };
-    // the building location of the node
-    sc2::Point2D location;
-
-    char status = PTNODE_TERMINAL;
-    char branchStatus = PTBRANCH_TERMINAL;
-    int numChildren = 0;
-    short level = 0;
-
-    // pointers to children nodes in the tree
-    PTNode* n, *s, *e, *w;
-
-} PTNode;
-
-// TODO: should we refactor so it uses a queue as an underlying data structure?
 class PlacementTree {
     public:
-    PlacementTree() {};
-    PlacementTree(sc2::Point2D loc, size_t maxLevels_, bool isArmyBuilding_, int xMargin_, int yMargin_)
-        { root = new PTNode(loc); maxLevels = maxLevels_; isArmyBuilding = isArmyBuilding_; xMargin = xMargin; yMargin = yMargin; };
+    sc2::Point2D findPlacement(sc2::Point2D root, std::vector< std::vector< bool >>& reservedTileMap, int maxDepth,
+                                int xMargin, int yMargin, bool isArmyBuilding);
 
-    // create n newNodes
-    // TODO: we need to use Query in addition to checking reserved tiles until we update reservedTiles to make unbuildable destroyables reserved
-    // we should reserve locations we find
-    void expand(std::vector< std::vector< bool >>& reservedTileMap, int newNodes = 8);
-
-    // if a node's location is invalid, prune the node and its children
-    void validate();
-    
-    // TODO: DFS might be best but BFS could be better for performance/memory reasons
-    sc2::Point2D getNextLocation();
-
-    protected:
-    // given a PTNode, delete it and its children
-    // TODO: do we need to give this function the parent of n instead?
-    PTNode* addNode(sc2::Point2D loc);
-    void pruneBranch(PTNode* n);
-
-    PTNode* root;
-    size_t maxLevels; // maximum height of tree
-    bool isArmyBuilding; // if true, then reserve space for addon as well
-
-    // these two define how much space we want between buildings
-    // if margin = 0, then there is no space between buildings. if margin = 1, siege tanks cant fit through the gap
-    int xMargin;
-    int yMargin;
-    // TODO: add instance variable here for the radius of the building (or perhaps safe to assume 1.5?)
-    float radius = 1.5;
+    private:
+    bool isValidPlacement(sc2::Point2D location, std::vector< std::vector< bool >>& reservedTileMap, bool isArmyBuilding);
 };
 
 } // end namespace Monte
