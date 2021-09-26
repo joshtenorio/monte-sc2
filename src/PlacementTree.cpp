0d8e4ee68b94b7dfb82c70073f49f54a0ebef199
Josh T <joshliam09@gmail.com>
2021-09-25 20:02:58 -0700

    feat: need to test and prove placement tree logic works

--- src/PlacementTree.cpp
+++ src/PlacementTree.cpp
@@ -4,12 +4,127 @@
 namespace Monte {
 
 sc2::Point2D PlacementTree::findPlacement(sc2::Point2D root, std::vector< std::vector< bool >>& reservedTileMap,
-                                        int maxDepth, int xMargin, int yMargin, bool isArmyBuilding){
-    // hehe
+                                        int depth, char parentDir, int xMargin, int yMargin, bool isArmyBuilding){
+    
+    // stopping condition
+    if(depth == 0 || !isValidPlacement(root, reservedTileMap, isArmyBuilding)){
+        // if we are in original function call and location is invalid, return tree full
+        if(parentDir == PT_DIR_NULL) return PT_TREE_FULL;
+        
+        // else 
+        return (isValidPlacement(root, reservedTileMap, isArmyBuilding) ? root : PT_NODE_NULL);
+    }
+        
+    
+    std::vector<sc2::Point2D> locations; // n_max = 5; root + 3-4 possible children
+    locations.resize(5);
+    locations.emplace_back(root);
+    sc2::Point2D nChild = sc2::Point2D(root.x, root.y+yMargin+(2*1.5));
+    sc2::Point2D sChild = sc2::Point2D(root.x, root.y-yMargin-(2*1.5));
+    sc2::Point2D eChild = sc2::Point2D(root.x+xMargin+(2*1.5)+2, root.y);
+    sc2::Point2D wChild = sc2::Point2D(root.x-xMargin-(2*1.5)-2, root.y);
+    if(!isArmyBuilding){ // TODO: double check this math
+        eChild.x -= 2;
+        wChild.x += 2;
+    }
+
+    switch(parentDir){
+    // FIXME: double check these directions
+        case PT_DIR_NULL: // get 4 children
+        locations.emplace_back(findPlacement(sChild, reservedTileMap, depth-1, PT_DIR_NORTH, xMargin, yMargin, isArmyBuilding));
+        locations.emplace_back(findPlacement(nChild, reservedTileMap, depth-1, PT_DIR_SOUTH, xMargin, yMargin, isArmyBuilding));
+        locations.emplace_back(findPlacement(wChild, reservedTileMap, depth-1, PT_DIR_EAST, xMargin, yMargin, isArmyBuilding));
+        locations.emplace_back(findPlacement(eChild, reservedTileMap, depth-1, PT_DIR_WEST, xMargin, yMargin, isArmyBuilding));
+        break;
+        case PT_DIR_NORTH: // get 3 children, not the north child
+        locations.emplace_back(findPlacement(sChild, reservedTileMap, depth-1, PT_DIR_NORTH, xMargin, yMargin, isArmyBuilding));
+        locations.emplace_back(findPlacement(wChild, reservedTileMap, depth-1, PT_DIR_EAST, xMargin, yMargin, isArmyBuilding));
+        locations.emplace_back(findPlacement(eChild, reservedTileMap, depth-1, PT_DIR_WEST, xMargin, yMargin, isArmyBuilding));
+        break;
+        case PT_DIR_SOUTH: // get 3 children, not south child
+        locations.emplace_back(findPlacement(nChild, reservedTileMap, depth-1, PT_DIR_SOUTH, xMargin, yMargin, isArmyBuilding));
+        locations.emplace_back(findPlacement(wChild, reservedTileMap, depth-1, PT_DIR_EAST, xMargin, yMargin, isArmyBuilding));
+        locations.emplace_back(findPlacement(eChild, reservedTileMap, depth-1, PT_DIR_WEST, xMargin, yMargin, isArmyBuilding));
+        break;
+        case PT_DIR_EAST: // get 3 children, not east child
+        locations.emplace_back(findPlacement(sChild, reservedTileMap, depth-1, PT_DIR_NORTH, xMargin, yMargin, isArmyBuilding));
+        locations.emplace_back(findPlacement(nChild, reservedTileMap, depth-1, PT_DIR_SOUTH, xMargin, yMargin, isArmyBuilding));
+        locations.emplace_back(findPlacement(wChild, reservedTileMap, depth-1, PT_DIR_EAST, xMargin, yMargin, isArmyBuilding));
+        break;
+        case PT_DIR_WEST: // get 3 children, not west child
+        locations.emplace_back(findPlacement(sChild, reservedTileMap, depth-1, PT_DIR_NORTH, xMargin, yMargin, isArmyBuilding));
+        locations.emplace_back(findPlacement(nChild, reservedTileMap, depth-1, PT_DIR_SOUTH, xMargin, yMargin, isArmyBuilding));
+        locations.emplace_back(findPlacement(eChild, reservedTileMap, depth-1, PT_DIR_WEST, xMargin, yMargin, isArmyBuilding));
+        break;
+    }
+
+    sc2::Point2D output = PT_NODE_NULL;
+    float distToRoot = std::numeric_limits<float>::max();
+    // find closest free location
+    for(auto& l : locations){
+        if(l == PT_NODE_NULL) continue;
+
+        float dist = sc2::DistanceSquared2D(root, l);
+        if(dist < distToRoot && isFreeLocation(l)) output = l;
+    }
+
+    // if node is null or we somehow get pt_tree_full, return tree full
+    if(output == PT_NODE_NULL || output == PT_TREE_FULL){
+        return PT_TREE_FULL;
+    }
+
+    // add location to cache, if in original function call
+    if(parentDir == PT_DIR_NULL)
+        cache.emplace_back(output);
+    return output;
+}
+
+void PlacementTree::clearCache(){
+    cache.clear();
 }
 
 bool PlacementTree::isValidPlacement(sc2::Point2D location, std::vector< std::vector< bool >>& reservedTileMap, bool isArmyBuilding){
+    // first use queryinterface to check if building location is valid
+    std::vector<sc2::QueryInterface::PlacementQuery> queries;
+    std::vector<bool> results;
+    queries.emplace_back(sc2::ABILITY_ID::BUILD_BARRACKS, location);
+    if(isArmyBuilding)
+        queries.emplace_back(sc2::ABILITY_ID::BUILD_SUPPLYDEPOT, sc2::Point2D(location.x + 2.5, location.y - 0.5));
     
+    results = gInterface->query->Placement(queries);
+    for(auto r : results)
+        if(!r)
+            return false;
+        
+    // then use reservedtilemap to check if we are reserving anything in that location
+    float radius = 1.5;
+    int xMin = location.x - radius, xMax = location.x + radius;
+    int yMin = location.y - radius, yMax = location.y + radius;
+
+    for(int x = xMin; x < xMax; x++){
+        for(int y = yMin; y < yMax; y++){
+            if(reservedTileMap[x][y]) return false;
+        }
+    }
+    // if army building, check the addon location too
+    if(isArmyBuilding){
+        float radius = 1;
+        int xMin = location.x+2.5 - radius, xMax = location.x+2.5 + radius;
+        int yMin = location.y-0.5 - radius, yMax = location.y-0.5 + radius;
+        for(int x = xMin; x < xMax; x++){
+            for(int y = yMin; y < yMax; y++){
+                if(reservedTileMap[x][y]) return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool PlacementTree::isFreeLocation(sc2::Point2D location){
+    for(auto c : cache)
+        if(c == location) return false;
+    
+    return true;
 }
 
 } // end namespace Monte
--- src/PlacementTree.h
+++ src/PlacementTree.h
@@ -8,6 +8,18 @@
 
 #include <sc2api/sc2_common.h>
 #include <vector>
+#include <limits>
+
+
+#define PT_TREE_FULL        sc2::Point2D(-1,-2) // if returned to buildingplacer, buildingplacer should find a new root position
+#define PT_NODE_NULL        sc2::Point2D(-1,-1) // location is phsyically invalid, don't search past this node
+
+
+#define PT_DIR_NULL         'X'
+#define PT_DIR_NORTH        'N'
+#define PT_DIR_SOUTH        'S'
+#define PT_DIR_EAST         'E'
+#define PT_DIR_WEST         'W'
 
 /*
 when needing to place a building using placementTree,
@@ -19,14 +31,28 @@ note: use recursion!
 by doing so we won't have to store anything long term :)))))
 */
 namespace Monte {
-
+// TODO: make findPlacement not static and store xMargin, yMargin, isArmyBuilding, perhaps root, etc
+// ESPECIALLY IF WE ARE USING MORE THAN ONE PLACEMENT TREES AT A TIME
 class PlacementTree {
     public:
-    sc2::Point2D findPlacement(sc2::Point2D root, std::vector< std::vector< bool >>& reservedTileMap, int maxDepth,
+    /*  precondition: root is the center of the building (ie, (4,4) or (4.5,4.5) )
+        parentDir is the direction of this node's parent
+    */
+    static sc2::Point2D findPlacement(sc2::Point2D root, std::vector< std::vector< bool >>& reservedTileMap, int depth, char parentDir,
                                 int xMargin, int yMargin, bool isArmyBuilding);
 
+    static void clearCache(); // clear cache when tree is full
+
     private:
-    bool isValidPlacement(sc2::Point2D location, std::vector< std::vector< bool >>& reservedTileMap, bool isArmyBuilding);
+
+    // checks if location is physically valid or is already reserved by something else
+    // precondition: location is the center of the building (ie, (4,4) or (4.5,4.5) )
+    static bool isValidPlacement(sc2::Point2D location, std::vector< std::vector< bool >>& reservedTileMap, bool isArmyBuilding);
+    
+    // returns true if location is not in cache
+    static bool isFreeLocation(sc2::Point2D location);
+
+    static std::vector<sc2::Point2D> cache;
 };
 
 } // end namespace Monte
