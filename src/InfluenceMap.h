06081d71279d1636063a7313c86cada46477fb31
joshtenorio <joshliam09@gmail.com>
2021-10-09 23:51:33 -0700

    feat: filled out first draft of influencemap class prototype, asked more questions of what influencemap should do

--- src/InfluenceMap.h
+++ src/InfluenceMap.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <pair>
+#include <vector>
 #include <sc2api/sc2_common.h>
 
 /*
@@ -9,11 +11,16 @@ a class InfluenceMap will have:
 - a radius
 then, influence map will get a list of sc2::Point2DIs that are within radius distance of center
 
+questions:
+- should gameobjects use InfluenceMap directly, or should we have child classes of InfluenceMap for each unit?
+- what if influence map took a pointer to ObservationInterface as a paramter in the constructor?
+- should influence map output an "ideal" point to move to st it is closest to target and lowest score?
+    or is this the responsibility of the object that has influencemap?
 
-onStep:
+
+update:
 0. update local map (ie, what points are in the map)
-1. generate InfluenceSource objects from local enemies/allies
-2. propagate said InfluenceSources across our map
+2. propagate InfluenceSources across our map
 3. return map to client (ie whoever has a influence map. likely some GameObject will have it)
 */
 namespace Monte {
@@ -21,11 +28,22 @@ namespace Monte {
         sc2::Point2DI center;
         float score;
         float maxRadius;
-        virtual float decay(float r) { return 0.0; };
     } InfluenceSource;
+
+    typedef std::pair<sc2::Point2DI, float> InfluenceTile;
 
     class InfluenceMap {
         public:
         InfluenceMap() {};
+        void update(sc2::Point2D center);
+        void addSource(sc2::Point2D center_, float score_, float radius_);
+        void clearSources();
+        sc2::Point2DI getOptimalWaypoint(sc2::Point2D target); // TODO: rename to getSafeWaypoint?
+
+        protected:
+        std::vector<InfluenceSource> sources;
+        std::vector<InfluenceTile> localRegion;
+        sc2::Point2DI center;
+        float maxRadius;
     };
 } // end namespace Monte
