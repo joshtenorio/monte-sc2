36830df1d3ae6e90c38fb7aaa1e5554c010e3952
joshtenorio <joshliam09@gmail.com>
2021-10-10 11:38:14 -0700

    fix: influence map now finds neighbors correctly

--- src/InfluenceMap.cpp
+++ src/InfluenceMap.cpp
@@ -71,14 +71,13 @@ void InfluenceMap::update(sc2::Point2D newCenter){
 }
 
 sc2::Point2D InfluenceMap::getOptimalWaypoint(sc2::Point2D target){
-    std::cout << "need optimal"  << std::endl;
     // 1. get all moore neighbors (should be 8 in total)
     // 2. disregard neighbors, that if we were to move to we would be further from the target
     std::vector<InfluenceTile> neighbors;
     for(auto& t : localRegion){
         // a tile is a neighbor if the squared distance to center is either 1 (in von neumann neighborhood only)
         // or 2 (in moore neighborhood)
-        if(sc2::DistanceSquared2D(t.first, center) == 1 || sc2::DistanceSquared2D(t.first, center) == 2){
+        if(sc2::DistanceSquared2D(t.first, center) <= 2){
             // only append if tile is closer to target than center is
             if(sc2::DistanceSquared2D(target, t.first) < sc2::DistanceSquared2D(target, center))
                 neighbors.emplace_back(t);
@@ -86,8 +85,10 @@ sc2::Point2D InfluenceMap::getOptimalWaypoint(sc2::Point2D target){
     }
     // 3. get the neighbor with lowest score, and return its location
     // this shouldn't be possible, but just in case something goes wrong
-    if(neighbors.empty())
+    if(neighbors.empty()){
+        std::cout << "neighbor empty" << std::endl;
         return sc2::Point2D(0,0);
+    }
     InfluenceTile waypoint = neighbors.front();
     for(auto& n : neighbors){
         if(n.second < waypoint.second) waypoint = n;
@@ -97,18 +98,20 @@ sc2::Point2D InfluenceMap::getOptimalWaypoint(sc2::Point2D target){
 }
 
 sc2::Point2D InfluenceMap::getSafeWaypoint(){
-    std::cout << "need safe"  << std::endl;
     // 1. get all moore neighbors (should be 8 in total)
     std::vector<InfluenceTile> neighbors;
     for(auto& t : localRegion){
         // a tile is a neighbor if the squared distance to center is either 1 (in von neumann neighborhood only)
         // or 2 (in moore neighborhood)
-        if(sc2::DistanceSquared2D(t.first, center) == 1 || sc2::DistanceSquared2D(t.first, center) == 2){
+        if(sc2::DistanceSquared2D(t.first, center) <= 2){
             neighbors.emplace_back(t);
         }
     }
-    if(neighbors.empty())
+    if(neighbors.empty()){
+        std::cout << "neighbor empty" << std::endl;
         return sc2::Point2D(0,0);
+    }
+        
     InfluenceTile waypoint = neighbors.front();
     for(auto& n : neighbors){
         if(n.second < waypoint.second) waypoint = n;
