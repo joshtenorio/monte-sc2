9f760fbdcbbccab805849cc7bb9622a86819e263
Josh T <joshliam09@gmail.com>
2021-10-13 15:23:53 -0700

    feat!: can identify ramp points, need to group them into ramps"

--- src/Ramp.cpp
+++ src/Ramp.cpp
@@ -2,106 +2,25 @@
 #include "Ramp.h"
 
 void findRamp(Ramp* r, sc2::Point2D start){
-    if(gInterface->observation->GetGameInfo().map_name == "2000 Atmospheres AIE"){
-        if(
-            sc2::DistanceSquared2D(start, sc2::Point2D(153, 144)) <
-            sc2::DistanceSquared2D(start, sc2::Point2D(71, 60))
-        ){ // use top right locations
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(153, 144));
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(150, 141));
-            r->barracksPos = sc2::Point2D(152.5, 141.5);
-            r->barracksWithAddonPos = sc2::Point2D(152.5, 141.5);
+    // get a list of points that are pathable but not buildable
+    // then, group together points that are close together to form a Ramp
+    // (reference how we generate mineral lines for this)
+    // then get the Ramp closest to start and generate barracks and supply depot positions from that
+    std::vector<sc2::Point2D> rampPoints;
+    sc2::Point2D playableMin = gInterface->observation->GetGameInfo().playable_min;
+    sc2::Point2D playableMax = gInterface->observation->GetGameInfo().playable_max;
+    for(int x = playableMin.x; x < playableMax.x; x++){
+        for(int y = playableMin.y; y < playableMax.y; y++){
+            sc2::Point2D p = sc2::Point2D(x, y);
+            if(gInterface->observation->IsPathable(p) && !gInterface->observation->IsPlacable(p)){
+                rampPoints.emplace_back(p);
+                sc2::Point3D p3d = sc2::Point3D(p.x, p.y, gInterface->observation->TerrainHeight(p));
+                gInterface->debug->debugDrawTile(p3d, sc2::Colors::Teal);
+            }
+                
         }
-        else{ // use bottom left locations
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(71, 60));
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(74, 63));
-            r->barracksPos = sc2::Point2D(71.5, 62.5);
-            r->barracksWithAddonPos = sc2::Point2D(69.5, 62.5);
-        }
-    } // end 2k atmospheres
-    else if(gInterface->observation->GetGameInfo().map_name == "Blackburn AIE"){
-        if(
-            sc2::DistanceSquared2D(start, sc2::Point2D(135, 46)) <
-            sc2::DistanceSquared2D(start, sc2::Point2D(49, 46))
-        ){ // use bottom right locations
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(135, 46));
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(138, 43));
-            r->barracksPos = sc2::Point2D(135.5, 43.5);
-            r->barracksWithAddonPos = sc2::Point2D(133.5, 43.5);
-        }
-        else{ // use bot left locations
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(49, 46));
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(46, 43));
-            r->barracksPos = sc2::Point2D(48.5, 43.5);
-            r->barracksWithAddonPos = sc2::Point2D(48.5, 43.5);
-        }
-    } // end blackburn
-    else if(gInterface->observation->GetGameInfo().map_name == "Jagannatha AIE"){
-        if(
-            sc2::DistanceSquared2D(start, sc2::Point2D(113, 146)) <
-            sc2::DistanceSquared2D(start, sc2::Point2D(55, 40))
-        ){ // use top right locations
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(113, 146));
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(110, 143));
-            r->barracksPos = sc2::Point2D(112.5, 143.5);
-            r->barracksWithAddonPos = sc2::Point2D(112.5, 143.5);
-        }
-        else{ // use bot left locations
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(55, 40));
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(58, 43));
-            r->barracksPos = sc2::Point2D(55.5, 42.5);
-            r->barracksWithAddonPos = sc2::Point2D(53.5, 42.5);
-        }
-    } // end jagannatha
-    else if(gInterface->observation->GetGameInfo().map_name == "Lightshade AIE"){
-        if(
-            sc2::DistanceSquared2D(start, sc2::Point2D(140, 47)) <
-            sc2::DistanceSquared2D(start, sc2::Point2D(44, 117))
-        ){ // use bot right locations
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(140, 47));
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(137, 50));
-            r->barracksPos = sc2::Point2D(137.5, 47.5);
-            r->barracksWithAddonPos = sc2::Point2D(135.5, 47.5);
-        }
-        else{ // use top left locations
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(44, 117));
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(47, 114));
-            r->barracksPos = sc2::Point2D(46.5, 116.5);
-            r->barracksWithAddonPos = sc2::Point2D(46.5, 116.5);
-        }
-    } // end lightshade
-    else if(gInterface->observation->GetGameInfo().map_name == "Romanticide AIE"){
-        if(
-            sc2::DistanceSquared2D(start, sc2::Point2D(154, 53)) <
-            sc2::DistanceSquared2D(start, sc2::Point2D(43, 116))
-        ){ // use bot right locations
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(154, 53));
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(157, 56));
-            r->barracksPos = sc2::Point2D(156.5, 53.5);
-            r->barracksWithAddonPos = sc2::Point2D(156.5, 53.5);
-        }
-        else{ // use top left locations
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(43, 116));
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(46, 119));
-            r->barracksPos = sc2::Point2D(43.5, 118.5);
-            r->barracksWithAddonPos = sc2::Point2D(41.5, 118.5);
-        }
-    } // end romanticide
-    else if(gInterface->observation->GetGameInfo().map_name == "Oxide AIE"){
-        if(
-            sc2::DistanceSquared2D(start, sc2::Point2D(130, 141)) <
-            sc2::DistanceSquared2D(start, sc2::Point2D(59, 60))
-        ){ // use top right locations
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(130, 141));
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(133, 144));
-            r->barracksPos = sc2::Point2D(132.5, 141.5);
-            r->barracksWithAddonPos = sc2::Point2D(132.5, 141.5);
-        }
-        else{ // use bot left locations
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(59, 60));
-            r->supplyDepotPoints.emplace_back(sc2::Point2D(62, 63));
-            r->barracksPos = sc2::Point2D(59.5, 62.5);
-            r->barracksWithAddonPos = sc2::Point2D(57.5, 62.5);
-        }
-    } // end oxide
+    }
+    gInterface->debug->sendDebug();
+
+
 }
--- src/Ramp.h
+++ src/Ramp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <queue>
 #include <sc2api/sc2_common.h>
 #include <sc2api/sc2_map_info.h>
 
@@ -9,6 +10,7 @@
 typedef struct Ramp_s_t {
     Ramp_s_t() {}
     bool isMainRamp = false;
+    std::vector<sc2::Point2D> rampPoints; // points that are in the ramp
     std::vector<sc2::Point2D> supplyDepotPoints;
     sc2::Point2D barracksPos;
     sc2::Point2D barracksWithAddonPos; // probably the more useful one
