445dcd36c8b9ab91033564befb1d2d9410a64fd2
Josh T <joshliam09@gmail.com>
2021-12-01 10:40:41 -0700

    chore: creating header file

---
+++ src/combat/Squad.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/*
+minimum functionality:
+- add/remove units
+- set target and order
+
+required classes/structs/enums:
+- Squad class
+- SquadOrder struct
+- GameObject struct (optional?)
+*/
