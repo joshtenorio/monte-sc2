19fa7c1c16bf477b7b112a38e728820991cb1316
Josh T <joshliam09@gmail.com>
2021-12-01 07:19:44 -0700

    chore: update bot version number

--- src/Bot.cpp
+++ src/Bot.cpp
@@ -2,7 +2,7 @@
 
 
 MarinePush* strategy; // this is file-global so i can delete it in OnGameEnd()
-std::string version = "v0_11_12"; // update this everytime we upload
+std::string version = "v0_11_13"; // update this everytime we upload
 
 // TODO: move this to header?
 std::vector<sc2::UNIT_TYPEID> depotTypes;
