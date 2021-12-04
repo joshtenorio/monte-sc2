e02a5693822ca53c4daf5b319313b1a61fc3a45a
Josh T <joshliam09@gmail.com>
2021-12-04 06:16:28 -0700

    feat: gameobject implemented, need to test

--- src/combat/GameObject.cpp
+++ src/combat/GameObject.cpp
@@ -1,1 +1,88 @@
 #include "GameObject.h"
+
+GameObject::GameObject(sc2::Tag gTag){
+    tag = gTag;
+}
+
+const sc2::Unit* GameObject::getUnit(){
+    return gInterface->observation->GetUnit(tag);
+}
+
+sc2::UnitTypeData GameObject::getData(){
+    return gInterface->observation->GetUnitTypeData()[getType()];
+}
+
+sc2::UnitTypeID GameObject::getType(){
+    if(!isValid()) return isValid();
+    return getUnit()->unit_type;
+}
+
+bool GameObject::isValid(){
+    return getUnit();
+}
+
+bool GameObject::isFriendly(){
+    if(!isValid()) return isValid();
+    return getUnit()->alliance == sc2::Unit::Alliance::Self;
+}
+
+bool GameObject::isFlying(){
+    if(!isValid()) return isValid();
+    return getUnit()->is_flying;
+}
+
+bool GameObject::isBuilding(){
+    if(!isValid()) return isValid();
+    return getUnit()->is_building;
+}
+
+bool GameObject::isVisible(){
+    if(!isValid()) return isValid();
+    return getUnit()->display_type == sc2::Unit::DisplayType::Visible;
+}
+
+bool GameObject::isLarva(){
+    if(!isValid()) return isValid();
+    return getUnit()->unit_type.ToType() == sc2::UNIT_TYPEID::ZERG_LARVA;
+}
+
+bool GameObject::isEgg(){
+    if(!isValid()) return isValid();
+    return getUnit()->unit_type.ToType() == sc2::UNIT_TYPEID::ZERG_EGG;
+}
+
+bool GameObject::move(sc2::Point2D target){
+    if(!isValid()) return isValid();
+    gInterface->actions->UnitCommand(getUnit(), sc2::ABILITY_ID::MOVE_MOVE, target);
+    return true;
+}
+
+bool GameObject::attack(sc2::Point2D target){
+    if(!isValid()) return isValid();
+    gInterface->actions->UnitCommand(getUnit(), sc2::ABILITY_ID::ATTACK_ATTACK, target);
+    return true;
+}
+
+bool GameObject::attack(const sc2::Unit* target){
+    if(!isValid()) return isValid();
+    gInterface->actions->UnitCommand(getUnit(), sc2::ABILITY_ID::ATTACK_ATTACK, target); // note: sc2::ABILITY_ID::ATTACK also exists
+    return true;
+}
+
+bool GameObject::rightClick(sc2::Point2D target){
+    if(!isValid()) return isValid();
+    gInterface->actions->UnitCommand(getUnit(), sc2::ABILITY_ID::SMART, target);
+    return true;
+}
+
+bool GameObject::rightClick(const sc2::Unit* target){
+    if(!isValid()) return isValid();
+    gInterface->actions->UnitCommand(getUnit(), sc2::ABILITY_ID::SMART, target);
+    return true;
+}
+
+bool GameObject::stop(){
+    if(!isValid()) return isValid();
+    gInterface->actions->UnitCommand(getUnit(), sc2::ABILITY_ID::STOP_STOP); // might be sc2::ABILITY_ID::STOP instead, idk
+    return true;
+}
--- src/combat/GameObject.h
+++ src/combat/GameObject.h
@@ -1,13 +1,31 @@
 #pragma once
 #include <sc2api/sc2_common.h>
 #include <sc2api/sc2_unit.h>
+#include <sc2api/sc2_data.h>
+#include "api.h"
 
 class GameObject {
     public:
     GameObject(sc2::Tag gTag);
 
     const sc2::Unit* getUnit();
+    sc2::UnitTypeData getData();
+    sc2::UnitTypeID getType();
+
     bool isValid();
+    bool isFriendly();
+    bool isFlying();
+    bool isBuilding();
+    bool isVisible();
+    bool isLarva();
+    bool isEgg();
+
+    bool move(sc2::Point2D target);
+    bool attack(sc2::Point2D target);
+    bool attack(const sc2::Unit* target);
+    bool rightClick(sc2::Point2D target);
+    bool rightClick(const sc2::Unit* target);
+    bool stop();
 
     protected:
     sc2::Tag tag;
